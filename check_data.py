3159d74d64e767906a80e28f8c934549b13b258d
Raoul Schram <r.d.schram@uu.nl>
2021-04-22 16:55:29 +0200

    Adjust check_data

--- check_data.py
+++ check_data.py
@@ -4,12 +4,18 @@ import json
 from pathlib import Path
 from pprint import pprint
 
+
 for data_dir in sorted(Path("packages").glob("[!.]*/")):
-    print(f"\n---------- {str(data_dir)} ----------")
     try:
-        with open(Path(data_dir, "download.json"), "r") as f:
+        download_fp = Path(data_dir, "download.json")
+        with open(download_fp, "r") as f:
             data = json.load(f)
-            pprint(data)
+            if data["success"]:
+                print(f"\n---------- {str(data_dir.name)} ----------")
+                readme_fp = Path(data_dir, "README.md")
+                with open(readme_fp, "r") as f:
+                    print(f.read())
+#                 print(data["success"], download_fp)
     except FileNotFoundError:
         print("Error finding 'download.json' in ", data_dir)
     try:
