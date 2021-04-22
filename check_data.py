6497143fc76f5045d2d466b63d0fdab5cbcf2e54
hailihu <hailihu@gmail.com>
2021-04-22 15:19:51 +0200

    Added info.json files for datasets 11-19 (#1)
    Add info.json

--- check_data.py
+++ check_data.py
@@ -4,7 +4,7 @@ import json
 from pathlib import Path
 from pprint import pprint
 
-for data_dir in Path("packages").glob("*/"):
+for data_dir in sorted(Path("packages").glob("[!.]*/")):
     print(f"\n---------- {str(data_dir)} ----------")
     try:
         with open(Path(data_dir, "download.json"), "r") as f:
@@ -12,3 +12,9 @@ for data_dir in Path("packages").glob("*/"):
             pprint(data)
     except FileNotFoundError:
         print("Error finding 'download.json' in ", data_dir)
+    try:
+        with open(Path(data_dir, "info.json"), "r") as f:
+            data = json.load(f)
+            pprint(data)
+    except FileNotFoundError:
+        print("Error finding 'info.json' in ", data_dir)
