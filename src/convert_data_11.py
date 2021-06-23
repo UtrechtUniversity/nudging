6a9c86e9653adbbb68cd07d24687ea922c801919
Haili Hu <hailihu@gmail.com>
2021-06-23 11:58:28 +0200

    Multi label classification (#5)
    * Apply multi-label classification
    
    * Added convert_all.py
    
    * Updated data.dvc
    
    * Put Database excel back in dvc
    
    * Adjust convert code to new data folder structure
    
    * use z-score threshold 0
    
    * updated data.dvc

--- src/convert_data_11.py
+++ src/convert_data_11.py
@@ -7,27 +7,40 @@ import numpy as np
 
 
 NUDGE_DOMAIN = 3
-NUDGE_TYPE = 3
+NUDGE_TYPE = [3, 7, 8]
+
+THRESHOLD = 0.
 
 
 def convert():
     """Convert raw csv"""
 
     # Raw dataset
-    filename = "data/raw/011_andreas/Commuter experiment_simple.csv"
+    filename = "data/external/011_andreas/Commuter experiment_simple.csv"
     # Copy csv file with raw data
-    copyfile(filename, "data/temp/raw_11.csv")
+    copyfile(filename, "data/raw/original_11.csv")
 
     # Get meand and std from control group
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
-        swipes = []
+        swipes = {}
         for row in reader:
             if row[0] == 'control':
-                swipes.append(int(row[9]))
-        swipes = np.array(swipes)
-        mean = swipes.mean()
-        std = swipes.std()
+                if row[2] == "1":
+                    # male
+                    gender = 1
+                elif row[2] == "8":
+                    # female
+                    gender = 0
+                else:
+                    gender = ""
+                if row[1] == " " or gender == "":
+                    continue
+                age = int(round(float(row[1])/10, 0))
+                if (age, gender) in swipes: 
+                    swipes[(age, gender)].append(int(row[9]))
+                else:
+                    swipes[(age, gender)] = [int(row[9])]
 
     # New dataset
     new_file = "data/processed/dataset_11.csv"
@@ -37,15 +50,10 @@ def convert():
         writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         with open(filename, newline='') as csvfile:
             reader = csv.reader(csvfile)
-            writer.writerow(
-                ["age", "gender", "nudge_domain", "nudge_type", "success", "z_score"])
+            # writer.writerow(
+            #     ["age", "gender", "nudge_domain", "nudge_type", "success", "z_score"])
             for row in reader:
                 if row[0] == 'nudge':
-                    z_score = round((int(row[9]) - mean)/std, 2)
-                    if float(row[9]) >= mean + std:
-                        success = 1
-                    else:
-                        success = 0
                     if row[2] == "1":
                         # male
                         gender = 1
@@ -54,8 +62,17 @@ def convert():
                         gender = 0
                     else:
                         gender = ""
-                    age = int(round(float(row[1]), 0))
-                    writer.writerow([age, gender, NUDGE_DOMAIN, NUDGE_TYPE, success, z_score])
+                    age = int(round(float(row[1])/10, 0))
+                    if (age, gender) in swipes:
+                        mean = np.array(swipes[(age, gender)]).mean() 
+                        std = np.array(swipes[(age, gender)]).std()
+
+                        z_score = round((int(row[9]) - mean)/std, 2)
+                        if float(row[9]) >= mean + std * THRESHOLD:
+                            success = 1
+                        else:
+                            success = 0
+                        writer.writerow([age, gender, NUDGE_DOMAIN, NUDGE_TYPE, success, z_score])
 
 
 if __name__ == "__main__":
--- src/convert_data_16.py
+++ src/convert_data_16.py
@@ -5,15 +5,16 @@ import pyreadstat
 import numpy as np
 
 NUDGE_DOMAIN = 3
-NUDGE_TYPE = 4
+NUDGE_TYPE = [4]
 
+THRESHOLD = 0.
 
 def convert():
     """Convert raw sav"""
 
-    input_data, _ = pyreadstat.read_sav('data/raw/016_vandenbroele/S2_OpenAccess.sav')
+    input_data, _ = pyreadstat.read_sav('data/external/016_vandenbroele/S2_OpenAccess.sav')
     # write csv file with raw data
-    input_data.to_csv("data/temp/raw_16.csv")
+    input_data.to_csv("data/raw/original_16.csv")
 
     person = np.array(input_data['ParticipantID'])
     visibility = np.array(input_data['Visibility'])
@@ -32,24 +33,43 @@ def convert():
     nudge = np.bitwise_and(np.bitwise_or(visibility == 1, pair == 1), exclude == 2)
 
     person_ids = np.unique(person)
-    ratios = []
+    ratios = {}
     # Get mean and std for control group
     for person_id in person_ids:
         person_index = person == person_id
+        index = np.bitwise_and(nudge, person_index)
         # individual control
-        meat_purchases_control = np.nansum(meat[np.bitwise_and(control, person_index)])
-        veg_purchases_control = np.nansum(veg[np.bitwise_and(control, person_index)])
-        ratio_control = veg_purchases_control/(meat_purchases_control + veg_purchases_control)
-        ratios.append(ratio_control)
-    mean = np.nanmean(ratios)
-    std = np.nanstd(ratios)
+        meat_purchases_control = np.nansum(meat[index])
+        veg_purchases_control = np.nansum(veg[index])
+        if (meat_purchases_control + veg_purchases_control) == 0:
+            continue
 
+        ratio_control = veg_purchases_control/(meat_purchases_control + veg_purchases_control)
+        age_list = age_array[index][np.isfinite(age_array[index])]
+        if len(age_list) > 0:
+            age = int(round(age_array[index][np.isfinite(age_array[index])][0]/10, 0))
+        else:
+            continue
+        gender = gender_array[index][np.isfinite(gender_array[index])][0]
+        if gender == 1:
+            gender = 0
+        elif gender == 0:
+            gender = 1
+        else:
+            gender = ""
+
+        if (age, gender) in ratios: 
+            ratios[(age, gender)].append(ratio_control)
+        else:
+            ratios[(age, gender)] = [ratio_control]
+    
     # New dataset
     new_file = "data/processed/dataset_16.csv"
     # Assume nudge_succes=1 when z_score>=1
     with open(new_file, mode='w') as outfile:
         writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
+        # writer.writerow(
+        #     ["age", "gender", "nudge_domain", "nudge_type", "success", "z_score"])
         # Determine nudge success
         for person_id in person_ids:
             person_index = person == person_id
@@ -59,10 +79,7 @@ def convert():
             if (meat_purchases + veg_purchases) == 0:
                 continue
             ratio = veg_purchases/(meat_purchases + veg_purchases)
-            z_score = (ratio - mean) / std
-            success = 1 if z_score >= 1.0 else 0
-            z_score = round(z_score, 2)
-            age = int(round(age_array[index][np.isfinite(age_array[index])][0], 0))
+            age = int(round(age_array[index][np.isfinite(age_array[index])][0]/10, 0))
             gender = gender_array[index][np.isfinite(gender_array[index])][0]
             if gender == 1:
                 gender = 0
@@ -70,6 +87,14 @@ def convert():
                 gender = 1
             else:
                 gender = ""
+
+            if (age, gender) in ratios:
+                mean = np.array(ratios[(age, gender)]).mean() 
+                std = np.array(ratios[(age, gender)]).std()
+                z_score = (ratio - mean) / std
+                success = 1 if z_score >= THRESHOLD else 0
+                z_score = round(z_score, 2)
+
             writer.writerow([age, gender, NUDGE_DOMAIN, NUDGE_TYPE, success, z_score])
 
 
