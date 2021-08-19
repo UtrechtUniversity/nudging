9f9a10d1ebb3b0b9e29d6311a5657ea0cf3adc21
Haili Hu <hailihu@gmail.com>
2021-08-19 10:54:43 +0200

    Pass predictors as argument

--- src/nudge_probability.py
+++ src/nudge_probability.py
@@ -6,30 +6,23 @@ import json
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import CategoricalNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 
 
-def get_probability(df):
-    """Calculate propensity score with logistic regression
+def get_probability(df, predictors):
+    """Calculate propability of nudge success with logistic regression
     Args:
         df (pandas.DataFrame):
-    
+        predictors (list): list with predictor variable names
     Returns:
-        pandas.DataFrame: dataframe with propensity score
+        pandas.DataFrame: dataframe with probabilities
     """
     T = 'success'
-    Y = ['education','hhi','ethnicity','political_party','SciKnow','MMS','CRT_ACC']
-    X = df.columns.drop([T]+Y)  
-    lg_model = LogisticRegression().fit(df[X].to_numpy().astype('int'), df[T].to_numpy().astype('int'))
+    # Drop rows with missing values for predictors
+    df_nonan = df.dropna(subset=predictors, inplace=False)
+    lr_model = LogisticRegression().fit(df_nonan[predictors].to_numpy().astype('int'), df_nonan[T].to_numpy().astype('int'))
 
-    data = df.assign(probability=lg_model.predict_proba(df[X])[:, 1])
+    data = df_nonan.assign(probability=lr_model.predict_proba(df_nonan[predictors])[:, 1])
     # write to file
     data.to_csv("data/processed/nudge_probabilty.csv", index=False)
 
@@ -44,9 +37,8 @@ def combine():
         dataframes.append(pd.read_csv(fin, encoding="iso-8859-1"))
     dataset = pd.concat(dataframes)
 
-    # Drop rows with missing values
+    # Replace missing values with Nans
     dataset.replace("", float("NaN"), inplace=True)
-    dataset.dropna(subset=["age", "gender"], inplace=True)
     dataset.to_csv("data/processed/combined.csv", index=False)
     return dataset
 
@@ -54,5 +46,6 @@ def combine():
 if __name__ == "__main__":
 
     combined_data = combine()
-    result = get_probability(combined_data)
+    predictors = ['age', 'gender', 'nudge_type', 'nudge_domain']
+    result = get_probability(combined_data, predictors)
     print(result)
