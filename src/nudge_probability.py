dbd699d53568bc7e5bca77e680ea2002b4d7b8d1
Haili Hu <hailihu@gmail.com>
2021-08-19 15:34:23 +0200

    get model accuracy

--- src/nudge_probability.py
+++ src/nudge_probability.py
@@ -21,12 +21,21 @@ def get_probability(df, predictors):
     # Drop rows with missing values for predictors
     df_nonan = df.dropna(subset=predictors, inplace=False)
     lr_model = LogisticRegression().fit(df_nonan[predictors].to_numpy().astype('int'), df_nonan[T].to_numpy().astype('int'))
-
     data = df_nonan.assign(probability=lr_model.predict_proba(df_nonan[predictors])[:, 1])
     # write to file
     data.to_csv("data/processed/nudge_probabilty.csv", index=False)
 
     return data
+
+def check_prob(df):
+    """Calculate accuracy of logistic regression model
+    Args:
+        df (pandas.DataFrame): dataframe with nudge success and probability
+    """
+    check = round(df['probability'])==df['success']
+    correct = sum(check)
+    total = check.shape[0]
+    print("Correct prediction of nudge success for {}% ({} out of {})". format(int(round(correct*100/total, 0)), correct, total))
 
 def combine():
     """Combine csv files"""
@@ -38,7 +47,10 @@ def combine():
     dataset = pd.concat(dataframes)
 
     # Replace missing values with Nans
-    dataset.replace("", float("NaN"), inplace=True)
+    dataset.replace("", pd.NA, inplace=True)
+
+    # Convert age to decades
+    dataset.age = (dataset.age/10).astype(int)
     dataset.to_csv("data/processed/combined.csv", index=False)
     return dataset
 
@@ -48,4 +60,6 @@ if __name__ == "__main__":
     combined_data = combine()
     predictors = ['age', 'gender', 'nudge_type', 'nudge_domain']
     result = get_probability(combined_data, predictors)
+
+    check_prob(result)
     print(result)
