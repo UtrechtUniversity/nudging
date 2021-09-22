ac2a9b9efdcda6a370246d43cf9ccf7f1f7afb24
Haili Hu <hailihu@gmail.com>
2021-09-22 15:53:56 +0200

    refactor

--- nudging/prepare.py
+++ nudging/prepare.py
@@ -3,10 +3,8 @@
 # pylint: disable=eval-used
 # pylint: disable=unused-import
 import glob
-
 import pandas as pd
 
-import nudging.propensity_score as ps
 from nudging.reader import Hotard, PennyCook1, Lieberoth, Simulated # noqa
 from nudging.utils import clean_dirs
 
@@ -22,14 +20,14 @@ def combine(infiles, outfile):
     for fin in infiles:
         print(fin)
         dataframes.append(pd.read_csv(fin, encoding="iso-8859-1"))
-    dataset = pd.concat(dataframes)
+    data = pd.concat(dataframes)
 
     # Replace missing values with Nans
-    dataset.replace("", pd.NA, inplace=True)
+    data.replace("", pd.NA, inplace=True)
 
     # Convert age to decades
-    dataset.age = (dataset.age/10).astype(int)
-    dataset.to_csv(outfile, index=False)
+    data.age = (data.age/10.).astype(int)
+    data.to_csv(outfile, index=False)
 
     print(f"Combined dataset in {outfile}")
 
@@ -50,43 +48,14 @@ if __name__ == "__main__":
 
     # Read and convert each dataset
     for name, path in datasets.items():
-
-        print("")
-        print("dataset ", name)
-        data = eval(name + "('" + path + "')")
+        print(f"\ndataset {name}")
+        dataset = eval(name + "('" + path + "')")
         # Write raw data to csv
-        data.write_raw("data/raw/" + name + ".csv")
+        dataset.write_raw("data/raw/" + name + ".csv")
 
-        # Convert data to standard format (with columns covariates, nudge, outcome)
-        df = data.standard_df
-        df.reset_index(drop=True, inplace=True)
-
-        # Apply OLS regression and print info
-        # print(smf.ols("outcome ~ nudge", data=df.apply(pd.to_numeric)).fit().summary().tables[1])
-
-        # calculate propensity score
-        df_ps = ps.get_pscore(df)
-
-        # Check if treatment and control groups are well-balanced
-        # ps.check_weights(df_ps)
-
-        # Plots
-        # ps.plot_confounding_evidence(df_ps)
-        # ps.plot_overlap(df_ps)
-
-        # Average Treatment Effect (ATE)
-        ps.get_ate(df_ps)
-
-        # propensity score weigthed ATE
-        ps.get_psw_ate(df_ps)
-
-        # propensity score matched ATE with CausalModel
-        # ps.get_psm_ate(df_ps)
-
-        # Cacculate nudge success and write to csv file
-        result = ps.match_ps(df_ps)
-        result = data.get_success(result)
-        data.write_interim(result, "data/interim/" + name + ".csv")
+        # Write interim data to csv
+        result = dataset.convert()
+        dataset.write_interim(result, "data/interim/" + name + ".csv")
 
     # combine separate csv files to one
     files = glob.glob('data/interim/[!combined.csv]*')
--- nudging/reader/base.py
+++ nudging/reader/base.py
@@ -1,8 +1,9 @@
 """Base class for DataSet """
 from abc import ABC, abstractmethod
-
 import numpy as np
 import pandas as pd
+
+import nudging.propensity_score as ps
 
 
 class BaseDataSet(ABC):
@@ -63,9 +64,52 @@ class BaseDataSet(ABC):
 
         return result
 
-    def get_success(self, data_frame):
+    def _get_success(self, data_frame):
         """Convert outcome to nudge success"""
         data_frame["success"] = self._compare(
             data_frame["outcome"], data_frame["control"])
         result = data_frame.drop(columns=["outcome", "control"])
+        return result
+
+    def convert(self):
+        """Convert raw dataset to standard-format csv file with nudge succes
+        Args:
+            name (str): name of reader
+            path (str): path to original dataset
+        Returns:
+            pandas.DataFrame: dataframe with nudge success for subjects in treatment group
+        """
+        # Convert data to standard format (with columns covariates, nudge, outcome)
+        data_frame = self.standard_df
+
+        data_frame.reset_index(drop=True, inplace=True)
+
+        # Apply OLS regression and print info
+        # print(
+        #    smf.ols("outcome ~ nudge",
+        # data=data_frame.apply(pd.to_numeric)).fit().summary().tables[1])
+
+        # calculate propensity score
+        df_ps = ps.get_pscore(data_frame)
+
+        # Check if treatment and control groups are well-balanced
+        # ps.check_weights(df_ps)
+
+        # Plots
+        # ps.plot_confounding_evidence(df_ps)
+        # ps.plot_overlap(df_ps)
+
+        # Average Treatment Effect (ATE)
+        ps.get_ate(df_ps)
+
+        # propensity score weigthed ATE
+        ps.get_psw_ate(df_ps)
+
+        # propensity score matched ATE with CausalModel
+        # ps.get_psm_ate(df_ps)
+
+        # Calculate nudge success
+        result = ps.match_ps(df_ps)
+        result = self._get_success(result)
+
         return result
