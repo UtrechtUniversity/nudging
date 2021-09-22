48de211e73be7a281af641039097e4eb7630a00c
Haili Hu <hailihu@gmail.com>
2021-09-22 16:56:46 +0200

    Added test_evaluate.py

---
+++ tests/test_evaluate.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from nudging.evaluate import evaluate_probabilities
+
+
+def test_evaluate_probabilities():
+
+    df = pd.DataFrame({
+		'probability':[0.6, 0.9, 0.1, 0.8, 0.4, 0.8, 0.4, 0.6, 0.3, 0.4],
+		'success': [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	}).reset_index()
+
+    result = evaluate_probabilities(df)
+    expected = 80
+    assert result == expected
+
